53bbd5b31f594f26ffb431828602f962287373f7
Lin Fei Kang <linfeikang123@gmail.com>
2022-01-09 18:35:29 -0500

    chore: Added comments to clarify the uses of the two Multiplicator constructors in DisplayMultiplications

--- multiplications/src/multiplications/DisplayMultiplications.java
+++ multiplications/src/multiplications/DisplayMultiplications.java
@@ -4,9 +4,11 @@ public class DisplayMultiplications {
 
 	public static void main(String[] args) {
 		
+		// use for getting multiplications in a range (e.g., 1 to 5)
 		// leftMin, leftMax, rightMin, rightMax
 //		Multiplicator multiplicator = new Multiplicator(1, 12, 1, 12);
-		int[] leftSelections = {4, 5, 11, 12};
+		// use for a subset of multiplications that doesn't fall into a range (e.g., 5 and 7)
+		int[] leftSelections = {12, 15};
 		Multiplicator multiplicator = new Multiplicator(leftSelections, 1, 12);
 		multiplicator.shuffleOperations();
 		multiplicator.printOperations(true);
